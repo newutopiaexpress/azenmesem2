03b2f1823359238a5d966a8b26fd8d68dccdda21
newutopiaexpress <basepaxton@gmail.com>
2024-12-22 16:37:42 +0100

    Initialize jrmy/live-sync-style-tweaks

---
+++ app/(chat)/chat/[id]/page.tsx
@@ -0,0 +1,64 @@
+import { cookies } from 'next/headers';
+import { notFound } from 'next/navigation';
+
+import { auth } from '@/app/(auth)/auth';
+import { Chat } from '@/components/chat';
+import { DEFAULT_MODEL_NAME, models } from '@/lib/ai/models';
+import { getChatById, getMessagesByChatId } from '@/lib/db/queries';
+import { convertToUIMessages } from '@/lib/utils';
+import { DataStreamHandler } from '@/components/data-stream-handler';
+
+export default async function Page(props: { params: Promise<{ id: string }> }) {
+  const params = await props.params;
+  const { id } = params;
+  const chat = await getChatById({ id });
+
+  if (!chat) {
+    notFound();
+  }
+
+  const session = await auth();
+
+  if (chat.visibility === 'private') {
+    if (!session || !session.user) {
+      return notFound();
+    }
+
+    if (session.user.id !== chat.userId) {
+      return notFound();
+    }
+  }
+
+  let selectedModelId: string = DEFAULT_MODEL_NAME;
+
+  const messagesFromDb = await getMessagesByChatId({
+    id,
+  });
+
+  const cookieStore = await cookies();
+  const modelIdFromCookie = cookieStore.get('model-id')?.value;
+  const selectedModel = models.find((model) => model.id === modelIdFromCookie);
+
+  if (selectedModel) {
+    const canUseModel =
+      !selectedModel.requiresAuth || (selectedModel.requiresAuth && session);
+
+    if (canUseModel) {
+      selectedModelId = selectedModel.id;
+    }
+  }
+
+  return (
+    <>
+      <Chat
+        id={chat.id}
+        initialMessages={convertToUIMessages(messagesFromDb)}
+        selectedModelId={selectedModelId}
+        user={session?.user}
+        selectedVisibilityType={chat.visibility}
+        isReadonly={session?.user?.id !== chat.userId}
+      />
+      <DataStreamHandler id={id} />
+    </>
+  );
+}
